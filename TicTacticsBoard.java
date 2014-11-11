f87640efa86838e56e0d3d85b177ef97c7f24791
Eunsong <eunsong.choi@gmail.com>
2014-11-10 19:23:37 -0500

    trying to add determination of unfinished draw

--- TicTacticsBoard.java
+++ TicTacticsBoard.java
@@ -36,21 +36,26 @@ public class TicTacticsBoard{
      *                      Evaluation methods                     *
      ***************************************************************/
     public int evaluate(){
+        int totalState = 0;
         // check diagonals
         int diagResult = checkDiags();
-        if ( diagResult != 0){
+        totalState += diagResult;
+        if ( diagResult != 0 && diagResult != -3 ){
             return diagResult;
         }
         int rowResult = 0;
         for ( int col = 0; col < 3; col++){
             rowResult = checkRow(col);
-            if ( rowResult != 0 ) return rowResult;
+            totalState += rowResult;
+            if ( rowResult != 0 && rowResult != -3 ) return rowResult;
         }
         int colResult = 0;
         for ( int row = 0; row < 3; row++){
             colResult = checkCol(row);
-            if ( colResult != 0 ) return colResult;
+            totalState += colResult;
+            if ( colResult != 0 && colResult != -3 ) return colResult;
         }
+        if ( totalState == -21 ) return -3; // draw (but the board is not fully occupied)
         return 0; // draw or undetermined        
     }
     private int checkDiags(){
@@ -60,21 +65,21 @@ public class TicTacticsBoard{
              (this.board[0][2].evaluate() == this.board[2][0].evaluate() ))){
             return this.board[1][1].evaluate();
         }
-        return 0;
+        return -3;
     }
     private int checkRow(int col){
         if ( this.board[0][col].evaluate() == this.board[1][col].evaluate() &&
              this.board[0][col].evaluate() == this.board[2][col].evaluate() ){
             return this.board[0][col].evaluate();
         }
-        return 0;
+        return -3;
     }
     private int checkCol(int row){
         if ( this.board[row][0].evaluate() == this.board[row][1].evaluate() &&
              this.board[row][0].evaluate() == this.board[row][2].evaluate() ){
             return this.board[row][0].evaluate();
         }
-        return 0;
+        return -3;
     }
     public int evaluateSubBoard(int row, int col){
         return this.board[row][col].evaluate();
